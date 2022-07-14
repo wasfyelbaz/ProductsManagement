4bf640e4df308a8c6030ddd384a87ee3ecac3da3
Wasfy Elbaz <wasfylbaz@gmail.com>
2022-07-14 16:25:45 -0700

    Added public dir

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+use Dotenv\Dotenv;
+
+require_once __DIR__ . '/../src/Support/helpers.php';
+require_once base_path() . 'vendor/autoload.php';
+require_once base_path() . 'routes/web.php';
+// composer dump-autoload
+
+$env = Dotenv::createImmutable(base_path());
+
+$env->load();
+
+app()->run();
