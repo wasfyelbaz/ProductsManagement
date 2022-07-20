4b4c91c6c03673303fc58578e043ad9ea3809ff0
Wasfy Elbaz <wasfylbaz@gmail.com>
2022-07-20 08:56:22 -0700

    Updated Application

--- src/Application.php
+++ src/Application.php
@@ -5,22 +5,45 @@ namespace elbaz;
 use elbaz\Http\Request;
 use elbaz\Http\Response;
 use elbaz\Http\Route;
-
+use elbaz\Database\DB;
+use elbaz\Database\Managers\MySQLManager;
 class Application
 {
     protected Route $route;
     protected Request $request;
     protected Response $response;
-
+    public DB $db;
     public function __construct()
     {
         $this->request = new Request;
         $this->response = new Response;
         $this->route = new Route($this->request, $this->response);
+        $this->db = new DB($this->getDatabaseDriver());
     }
+    public function getDatabaseDriver()
+    {
+        if (strcmp(env('DB_DRIVER'), 'mysql') == 0 )
+        {
+            return new MySQLManager;
+        }
+    }
+    protected function loadConfigurations()
+    {
+        foreach(scandir(config_path()) as $file) {
+            if ($file == '.' || $file == '..') {
+                continue;
+            }
+            $filename = explode('.', $file)[0];
 
+            yield $filename => require config_path() . $file;
+        }
+
+    }
     public function run()
     {
+        // initialize db
+        $this->db->init();
+        // resolve routes
         $this->route->resolve();
     }
 
